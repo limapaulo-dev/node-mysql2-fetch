e3b8e73ddef6ae26aa17ecc83411a1af23183d42
Paulo Lima <limapaulo.dev@gmail.com>
2023-08-26 19:30:34 -0300

    setup mysql

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
+const mysql = require('mysql2')
+
 const inputs = document.querySelectorAll('input.form');
 
 const cep = document.querySelector('#cep');
@@ -5,6 +7,38 @@ const logradouro = document.querySelector('#logradouro');
 const bairro = document.querySelector('#bairro');
 const localidade = document.querySelector('#localidade');
 const uf = document.querySelector('#uf');
+
+const dbOpt = {
+    url : 'localholst',
+    user: 'root',
+    pw : 'mysqlpw',
+    db: 'node_mysql'
+}
+
+const getConn = (mysql) => {
+    const conn = mysql.createConnection(dbOpt);
+
+    conn.connect((err)=>{
+        if (err) throw console.log(err);
+
+        const createTBAdress = `create table if not exists cep (
+            id integer auto_increment not null primary key,
+            cep integer(9) not null unique,
+            logradouro varchar(255),
+            bairro varchar(255),
+            cidade varchar(25),
+            uf char(2)
+        )`
+
+        conn.query(createTBAdress, (err, result)=>{
+            console.log(err)
+            console.log(result)
+        });
+
+    });
+
+
+}
 
 cep.addEventListener('blur', async () => {
   let search = cep.value.replace('-', '');
@@ -25,7 +59,10 @@ cep.addEventListener('blur', async () => {
     bairro.value = data.bairro;
     localidade.value = data.localidade;
     uf.value = data.uf;
-  }
+
+    getConn(mysql)
+   
+  }  
 
   console.log(res);
   console.log(inputs);
