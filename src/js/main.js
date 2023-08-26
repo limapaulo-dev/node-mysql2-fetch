ef2ed1c3c38716a76054ca4c61fcb2d55e891de4
Paulo Lima <limapaulo.dev@gmail.com>
2023-08-26 19:03:54 -0300

    setup cep api

---
+++ src/js/main.js
@@ -0,0 +1,33 @@
+const inputs = document.querySelectorAll('input.form');
+
+const cep = document.querySelector('#cep');
+const logradouro = document.querySelector('#logradouro');
+const bairro = document.querySelector('#bairro');
+const localidade = document.querySelector('#localidade');
+const uf = document.querySelector('#uf');
+
+cep.addEventListener('blur', async () => {
+  let search = cep.value.replace('-', '');
+  
+  const options = {
+    method: 'get',
+    mode: 'cors',
+    cache: 'default',
+  };
+
+  const res = await fetch(`https://viacep.com.br/ws/${search}/json/`, options).catch((err) => console.log(err));
+
+  if (res) {
+    const data = await res.json().catch((err) => console.log(err));
+    console.log(data);
+
+    logradouro.value = data.logradouro;
+    bairro.value = data.bairro;
+    localidade.value = data.localidade;
+    uf.value = data.uf;
+  }
+
+  console.log(res);
+  console.log(inputs);
+  console.log(cep);
+});
